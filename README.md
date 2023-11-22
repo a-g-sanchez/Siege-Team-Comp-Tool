8f292e1ef2c5f0d3491f6db86870452e00fcc1e5
a-g-sanchez <aarongsanchez@google.com>
2023-11-22 15:23:52 -0600

    Added back in the primitive background color in the theme as well as started on the README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Siege Team Comp Tool
+
+Siege Team Comp Tool is a CLI tool written in Golang geared towards helping competivite Rainbow 6: Siege teams organize their in game team composition.
+
+## As a User
--- initializers/setTheme.go
+++ initializers/setTheme.go
@@ -7,6 +7,7 @@ import (
 
 func SetTheme() {
 	tview.Styles = tview.Theme{
+		PrimitiveBackgroundColor:    tcell.ColorBlack,
 		ContrastBackgroundColor:     tcell.ColorDarkBlue,
 		MoreContrastBackgroundColor: tcell.ColorGreen,
 		BorderColor:                 tcell.ColorWhite,
@@ -15,7 +16,7 @@ func SetTheme() {
 		PrimaryTextColor:            tcell.ColorGhostWhite,
 		SecondaryTextColor:          tcell.ColorSalmon,
 		TertiaryTextColor:           tcell.ColorGreen,
-		InverseTextColor:            tcell.ColorDeepSkyBlue,
+		InverseTextColor:            tcell.ColorDarkSlateBlue,
 		ContrastSecondaryTextColor:  tcell.ColorDarkCyan,
 	}
 }
