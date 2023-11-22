f4b96f20fd865dbf0d08a296d276f3ce2f145061
a-g-sanchez <aarongsanchez@google.com>
2023-11-22 13:19:50 -0600

    Removed unused code and moved the theme reset to a new file. Last commit was wrong and the display is NOW fully functional

--- main.go
+++ main.go
@@ -4,33 +4,15 @@ import (
 	"fmt"
 
 	"github.com/a-g-sanchez/hackathon-project/initializers"
-	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-func configStyles() {
-	tview.Styles = tview.Theme{
-		// PrimitiveBackgroundColor:    tcell.ColorBlack,
-		ContrastBackgroundColor:     tcell.ColorDarkBlue,
-		MoreContrastBackgroundColor: tcell.ColorGreen,
-		BorderColor:                 tcell.ColorWhite,
-		TitleColor:                  tcell.ColorWhite,
-		GraphicsColor:               tcell.ColorWhite,
-		PrimaryTextColor:            tcell.ColorGhostWhite,
-		SecondaryTextColor:          tcell.ColorSalmon,
-		TertiaryTextColor:           tcell.ColorGreen,
-		InverseTextColor:            tcell.ColorDeepSkyBlue,
-		ContrastSecondaryTextColor:  tcell.ColorDarkCyan,
-	}
-}
-
 func init() {
-	configStyles()
+	initializers.SetTheme()
 }
 
 func main() {
 	stratData := initializers.SetData()
-	// fmt.Println(stratData)
 
 	app := tview.NewApplication()
 
@@ -56,11 +38,9 @@ func main() {
 	list.SetSelectedFunc(func(idx int, _ string, _ string, _ rune) {
 		var str string
 		if idx < len(stratData) {
-			// str += fmt.Sprintf("\n")
 			for _, site := range stratData[idx].Sites {
 				str += fmt.Sprintf("%s\n %s\n\n", site.Name, site.Operators)
 			}
-
 		}
 		infoDisplay.SetText(str)
 
